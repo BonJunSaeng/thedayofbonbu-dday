d68a1b15be60b8e85ef53fd0a0b0be7c572d72bd
Daeho Han <devhoho@hoMac.local>
2019-12-18 10:57:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# [The Day Of Bonbu - D Day]
+
+## Copyright and License
+
+Copyright 2013-2019 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-coming-soon/blob/gh-pages/LICENSE) license.
---
+++ js/coming-soon.js
@@ -0,0 +1,29 @@
+(function ($) {
+  "use strict"; // Start of use strict
+  var ddaytimer = setInterval(dayGap, 100);
+
+  function dayGap() {
+    var dday = new Date("January 16, 2020 00:00:00").getTime();
+    var nowday = new Date();
+    nowday = nowday.getTime();
+    var distance = dday - nowday;
+
+    var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    var h = Math.floor((distance / (1000 * 60 * 60)) % 24);
+    var m = Math.floor((distance / (1000 * 60)) % 60);
+    var s = Math.floor((distance / 1000) % 60);
+
+    if (distance <= 0) {
+      document.getElementById("dday-timer").innerHTML = "D-day";
+    } else {
+      document.getElementById("time-day").innerHTML = d;
+      document.getElementById("time-hour").innerHTML = h;
+      document.getElementById("time-minute").innerHTML = m;
+      document.getElementById("time-second").innerHTML = s;
+    }
+  }
+
+  // No JS
+
+})(jQuery); // End of use strict
